bd2b573bcd6ef3ddcfb9bbbcc7d475f1f15a8a46
Alex Barbera <alex_barbera.m@hotmail.com>
2025-04-10 17:08:10 -0400

    Now with skeletons

--- src/models.py
+++ src/models.py
@@ -1,16 +1,38 @@
 import torch
+import torch.nn.functional as F
 
 
 class MNISTFCClassifier(torch.nn.Module):
     def __init__(self):
-        self.network = torch.nn.Sequential([
+        super(MNISTFCClassifier, self).__init__()
+        self.network = torch.nn.Sequential(
             torch.nn.Linear(512,256),
             torch.nn.ReLU(),
-            torch.nn.Linear(256, 128)
+            torch.nn.Linear(256, 128),
             torch.nn.ReLU(),
             torch.nn.Linear(128, 10),
             torch.nn.LogSoftmax()
-        ])
+        )
 
     def forward(self, x):
         return self.network(x)
+
+
+class CIFARClassifier(torch.nn.Module):  # from pytorch example
+    def __init__(self):
+        super(CIFARClassifier, self).__init__()
+        self.conv1 = torch.nn.Conv2d(3, 6, 5)
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.conv2 = torch.nn.Conv2d(6, 16, 5)
+        self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
--- src/training.py
+++ src/training.py
@@ -1,6 +1,8 @@
 import  time
 import argparse
+from typing import List
 
+import numpy
 import torch
 import torchvision
 import sys
@@ -13,6 +15,7 @@ def parse_args():
 
     output.add_argument("epochs", type=int)
     output.add_argument("batch-size", type=int)
+    output.add_argument("model", choices=["mnist", "cifar", "reduced-mnist", "reduced-cifar"])
 
     return output.parse_args(sys.argv[1:])
 
@@ -33,6 +36,49 @@ def get_mnist():
                                        ))
 
     return train, test
+
+
+def get_cifar():
+    train = torchvision.datasets.CIFAR10("/tmp/cifar",
+                                       transform=torchvision.transforms.Compose(
+                                           [
+                                               torchvision.transforms.ToTensor(),
+                                               torchvision.transforms.Normalize(
+                                                   (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+                                               )
+                                           ]
+                                       ))
+
+    test = torchvision.datasets.CIFAR10("/tmp/cifar", train=False,
+                                       transform=torchvision.transforms.Compose(
+                                           [
+                                               torchvision.transforms.ToTensor(),
+                                               torchvision.transforms.Normalize(
+                                                   (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+                                               )
+                                           ]
+                                       ))
+
+    return train, test
+
+
+def get_index_of_outliers_iqr(model: torch.nn.Module, data: torch.utils.data.Dataset, loss_fn: torch.nn.Module,
+                              factor: float = 1.5) -> List[int]:
+    output = list()
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in data:
+            yp = model(x)
+            l = loss_fn(yp, y).detach().item()
+            output.expand(l)
+
+    m = numpy.median(output)
+    iq1 = numpy.median(output[output < m])
+    iq3 = numpy.median(output[output > m])
+    iqr = (iq3 - iq1) * factor
+
+    return numpy.where(output > iq3 + iqr)
 
 
 def normal_train_loop(model, dataloader, optimizer, loss_fn):
@@ -80,17 +126,91 @@ def train_normal_mnist(args):
         t = time.time()
         train_loss = normal_train_loop(model, train, optimizer, torch.nn.NLLLoss())
         t = time.time() - t
-        sw.add_scalar("loss/normal/train", train_loss, epoch)
-        sw.add_scalar("time/normal/train", t, epoch)
+        sw.add_scalar("loss/normal_mnist/train", train_loss, epoch)
+        sw.add_scalar("time/normal_mnist/train", t, epoch)
 
         t = time.time()
         test_loss = test_loop(model, test, torch.nn.NLLLoss())
         t = time.time() - t
-        sw.add_scalar("loss/normal/test", train_loss, epoch)
-        sw.add_scalar("time/normal/test", t, epoch)
+        sw.add_scalar("loss/normal_mnist/test", test_loss, epoch)
+        sw.add_scalar("time/normal_mnist/test", t, epoch)
+
+
+def train_reduced_mnist(args):
+    train, test = get_mnist()
+
+    #train = torch.utils.data.Dataloader(train, batch_size=args.batch_size, shuffle=False, num_workers=2)
+    test = torch.utils.data.Dataloader(test, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+
+    model = models.MNISTFCClassifier()
+    optimizer = torch.optim.SGD(model.parameters())
+
+    sw = SummaryWriter(args.tensorboard_path)
+
+    for epoch in range(args.epochs):
+        train = torch.utils.data.Subset(train, list(range(len(train))))
+        t = time.time()
+        train_loss = normal_train_loop(model, train, optimizer, torch.nn.NLLLoss())
+        t = time.time() - t
+        sw.add_scalar("loss/reduced_mnist/train", train_loss, epoch)
+        sw.add_scalar("time/reduced_mnist/train", t, epoch)
+
+        t = time.time()
+        train = torch.utils.data.Subset(train,
+                                        get_index_of_outliers_iqr(model, train, torch.nn.NLLLoss(),
+                                                                  args.iqr_factor)
+                                        )
+        t = time.time() - t
+        sw.add_scalar("time/reduced_mnist/index", t, epoch)
+
+        t = time.time()
+        train_loss = normal_train_loop(model, train, optimizer, torch.nn.NLLLoss())
+        t = time.time() - t
+        sw.add_scalar("loss/reduced_mnist/train_reduced", train_loss, epoch)
+        sw.add_scalar("time/reduced_mnist/train_reduced", t, epoch)
+
+        t = time.time()
+        test_loss = test_loop(model, test, torch.nn.NLLLoss())
+        t = time.time() - t
+        sw.add_scalar("loss/reduced_mnist/test", test_loss, epoch)
+        sw.add_scalar("time/reduced_mnist/test", t, epoch)
+
+
+def train_normal_cifar(args):
+    train, test = get_cifar()
+
+    train = torch.utils.data.Dataloader(train, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    test = torch.utils.data.Dataloader(test, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+    model = models.CIFARClassifier()
+    optimizer = torch.optim.SGD(model.parameters())
+
+    sw = SummaryWriter(args.tensorboard_path)
+
+    for epoch in range(args.epochs):
+        t = time.time()
+        train_loss = normal_train_loop(model, train, optimizer, torch.nn.NLLLoss())
+        t = time.time() - t
+        sw.add_scalar("loss/normal_cifar/train", train_loss, epoch)
+        sw.add_scalar("time/normal_cifar/train", t, epoch)
+
+        t = time.time()
+        test_loss = test_loop(model, test, torch.nn.NLLLoss())
+        t = time.time() - t
+        sw.add_scalar("loss/normal_cifar/test", test_loss, epoch)
+        sw.add_scalar("time/normal_cifar/test", t, epoch)
 
 
 if __name__ == "__main__":
     args = parse_args()
     print(args)
-    train_normal_mnist(args)
+
+    if args.model == "mnist":
+        train_normal_mnist(args)
+    elif args.model == "cifar":
+        train_normal_cifar(args)
+    elif args.model == "reduced-mnist":
+        train_reduced_mnist(args)
+    elif args.model == "reduced-cifar":
+        train_reduced_cifar(args)
